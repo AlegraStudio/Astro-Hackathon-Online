1969d007229338ebebb99bb3989e20e43a209838
AlegraStudio <152314375+AlegraStudio@users.noreply.github.com>
2023-12-08 13:58:20 -0500

    Update app.jsx

--- bootcamp/src/bootcamp_frontend/src/app.jsx
+++ bootcamp/src/bootcamp_frontend/src/app.jsx
@@ -1,22 +1,35 @@
+// Importación de las bibliotecas necesarias: React y Chakra UI.
+// React se usa para construir componentes y manejar el estado.
+// Chakra UI proporciona componentes de UI listos para usar y personalizables.
 import * as React from "react";
 import { Button } from "@chakra-ui/react";
-import EmpleadoAdmin from "./empleadoAdmin.jsx";
 
+// Importación de un componente personalizado para la administración de estudiantes.
+import EstudianteAdmin from "./estudianteAdmin.jsx";
+
+// Definición del componente funcional App.
 const App = () => {
-  const [empleadoSearch, setEmpleadoSearch] = React.useState(0);
+  // Definición de un estado local 'estudianteSearch' para controlar la visualización de la interfaz.
+  // El estado inicial es 0, lo que indica que no se está visualizando la administración de estudiantes.
+  const [estudianteSearch, setEstudianteSearch] = React.useState(0);
 
   return (
+    // Renderización condicional basada en el estado 'estudianteSearch'.
+    // Si 'estudianteSearch' es 1, se muestra el componente EstudianteAdmin.
+    // De lo contrario, se muestra un botón para cambiar a la vista de administración.
     <>
-      {empleadoSearch === 1 ? (
-        <EmpleadoAdmin />
+      {estudianteSearch === 1 ? (
+        <EstudianteAdmin />
       ) : (
+        // Sección de bienvenida con un botón para cambiar a la vista de administración.
         <>
-        <h1>Entrar</h1>
-        <Button colorScheme='red' onClick={() => setEmpleadoSearch(1)}>Admin</Button>
+          <h1>Entrar</h1>
+          <Button colorScheme='red' onClick={() => setEstudianteSearch(1)}>Admin</Button>
         </>
       )}
     </>
   );
 };
 
+// Exportación del componente App para su uso en otros archivos.
 export default App;
